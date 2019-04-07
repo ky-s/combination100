994a5943cd5184f60fbc14aa8dac381851a840c5
ky-s <kyohei_86@hotmail.com>
2019-04-07 09:40:01 +0900

    julia solved

---
+++ combination100.jl
@@ -0,0 +1,8 @@
+function patterns(pat = "1", i = 1, arr = collect(2:9), ans = [])
+    length(arr) < i && return push!(ans, pat)
+    map(op -> patterns(pat * op * string(arr[i]), i + 1, arr, ans), ("+", "-", ""))
+    return ans
+end
+
+@time ans = filter(x -> eval(Meta.parse(x)) == 100, patterns())
+println(ans)
--- combination100.rb
+++ combination100.rb
@@ -1,7 +1,6 @@
 def patterns(str = "1", i = 0, arr = (2..9).to_a)
   i >= arr.size and return str
-  x = arr[i].to_s; i += 1
-  ['+', '-', ''].map { |op| patterns("#{str}#{op}#{x}", i, arr) }.flatten
+  ['+', '-', ''].map { |op| patterns("#{str}#{op}#{arr[i]}", i + 1, arr) }.flatten
 end
 
 ans = patterns.select { |p| eval(p) == 100 }
